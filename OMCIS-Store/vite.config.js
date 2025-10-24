ef81cfdd195c4affbae0d5aa380c366ff0c93d4a
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-10-24 19:26:00 -0300

    Corrige 'base' do Vite e 'working-directory' do Actions

--- OMCIS-Store/vite.config.js
+++ OMCIS-Store/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ADICIONE ESTA LINHA:
-  base: '/OMCIS-Store/' 
-})
+  // 1. ADICIONE ESTA LINHA
+  base: "/OMCIS-Store/",
+});
