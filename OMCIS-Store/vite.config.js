2a128b98b39c5f9e1a9250e989fb699a3b1786e3
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-10-24 19:16:31 -0300

    corrigi o erro do deploy

--- OMCIS-Store/vite.config.js
+++ OMCIS-Store/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // ADICIONE ESTA LINHA:
-  base: "/OMCIS-Store/",
-  // Troque 'NOME-DO-SEU-REPOSITORIO' pelo nome exato do seu repo no GitHub
-});
+  base: '/OMCIS-Store/' 
+})
